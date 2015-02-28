531c9598b5194a89c3a3d6e4fc67cc0fb32cd29f
Kaito <kaaitokid@gmail.com>
2015-02-28 14:33:26 +0100

    pics and bg started

--- penguinRush/Background.hpp
+++ penguinRush/Background.hpp
@@ -1,10 +1,22 @@
 #ifndef BACKGROUND_HPP
 #define BACKGROUND_HPP
 
-class Background
-{
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Background {
+
+private:
+    sf::Sprite sprite;
+    sf::Texture texture;
+
+
 public:
     Background();
+
+    void update(float deltatime);
+
+    void draw(sf::RenderWindow& window);
 };
 
 #endif // BACKGROUND_HPP
